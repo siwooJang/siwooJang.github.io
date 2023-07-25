a4e7a42fd575c1f9c86d507a1ac1727039bb5825
siwooJang <joy8417@naver.com>
2023-07-25 23:31:35 +0900

    sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://siwoojang.github.io",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-      cacheDir: "./.cache/image",
-      logLevel: "debug",
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp",
+    cacheDir: "./.cache/image",
+    logLevel: "debug"
+  })]
 });
